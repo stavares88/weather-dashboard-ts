81a33dd2191684907d3414a1209abb90cd6af32d
Steven Tavares <stavares88@gmail.com>
2026-08-12 11:14:50 -0600

    Add weather details and error handling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,15 +18,22 @@ const apiKey = import.meta.env.VITE_API_KEY;
 function SearchBar() {
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     const handleSearch = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
         const response = await fetch(url);
 
-        const data = await response.json();
+        if (!response.ok) {
+        setError("City not found");
+        setWeatherData(null);
+        return;
+    }
 
+        const data = await response.json();
+        console.log(data);
         setWeatherData(data);
+        setError(null);
     };
 
     return (
@@ -46,8 +53,19 @@ function SearchBar() {
     <div>
         <h2>{weatherData.name}</h2>
         <p>Temperature: {weatherData.main.temp}</p>
+        <p>Humidity: {weatherData.main.humidity}%</p>
+         <p>Weather: {weatherData.weather[0].description}</p>
+
+
     </div>
-)}
+    )}
+
+        {error && (
+            <div>
+                {error}
+            </div>
+        )}
+
         </div>
     );
 }
